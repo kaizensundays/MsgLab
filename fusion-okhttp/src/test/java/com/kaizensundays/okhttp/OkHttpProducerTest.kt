4e06ce3bafb6b4cd7714e3a02020fafc24f3568c
KaizenSundays <kaizensundays@localhost>
2025-02-15 17:42:25 -0500

    [ fusion ] Spring Boot 3.4.2

--- fusion-okhttp/src/test/java/com/kaizensundays/okhttp/OkHttpProducerTest.kt
+++ fusion-okhttp/src/test/java/com/kaizensundays/okhttp/OkHttpProducerTest.kt
@@ -1,6 +1,8 @@
 package com.kaizensundays.okhttp
 
 import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Created: Sunday 10/1/2023, 1:37 PM Eastern Time
@@ -9,7 +11,11 @@ import org.junit.Test
  */
 class OkHttpProducerTest {
 
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     @Test
     fun test() {
+        logger.info("Ok")
     }
+
 }
